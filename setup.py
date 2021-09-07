8f0ca0202c2ca50b26c01eeef795cb57ef8d9dc2
Jose Mielgo <mielgosez@gmail.com>
2021-09-07 10:30:58 +0200

    Upgrading to version 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="conga_office",
-    version="0.1.0",
+    version="0.2.0",
     author="Jose Mielgo",
     author_email="mielgosez@gmail.com",
     description="Python library thought to active conga-office in order to appear online to your employer while "
